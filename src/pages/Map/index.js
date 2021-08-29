c927d80e1d6e483a384f6f8cec1bc0c4647b90be
tonglei <ltl_code@163.com>
2021-08-29 14:09:18 +0800

    edit

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './index.scss';
 
 class Map extends React.Component{
+
+    componentDidMount(){
+        // 创建地图实例
+        const map = new window.BMap.Map('container');
+        //设置中心坐标
+        const point = new window.BMap.Point(116.404, 39.915);
+        //初始化地图，设置展示级别
+        map.centerAndZoom(point, 15)
+    }
+
     render(){
         return(
-            <div>Map</div>
+            <div className="map">
+                <div id="container"></div>
+            </div>
         )
     }
 }
